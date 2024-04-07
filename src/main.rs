a50db23547ad2240fe343f3832b30a8099db5743
FilipoDEV <68162426+FilipoDEV@users.noreply.github.com>
2024-04-07 05:01:27 +0000

    Oao

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn resolve_equation(a: i32, b: i32, c: i32) -> (String, i32) {
+  // TODO: terminar de escrever
+  return (String::new("oao"), 2)
+}
+
+fn main () {
+    println!("Calculadora de equação do segundo grau:")
 }
