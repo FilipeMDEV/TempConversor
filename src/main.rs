b2fc4efc1cbb26d659fb01f5dd7cbf2c179bb003
Filipe Menezes <68162426+FilipoDEV@users.noreply.github.com>
2024-04-16 12:57:47 +0000

    conversor pronto yaaaaaaayyyy

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
-fn fahrenheit_to_celsius(fahrenheit: String) -> f64 {
-  let fahrenheit: f64 = fahrenheit.trim().parse().expect("Deve ser um número");
+fn get_number_chose() -> i8 {
+  let mut option = String::new();
+  std::io::stdin().read_line(&mut option).expect("Falha ao ler option");
+  let option: i8 = option.trim().parse().expect("Deve ser um número");
+  match option {
+    1 | 2 => option,
+    _ => {
+      println!("Deve ser 1 ou 2");
+      get_number_chose()
+    }
+  }
+}
+
+fn get_temperature_chose() -> f64 {
+  let mut temperature = String::new();
+  std::io::stdin().read_line(&mut temperature).expect("Falha ao ler");
+  let temperature: f64 = temperature.trim().parse().expect("Deve ser um número");
+  temperature
+}
+
+fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
   let celsius = (fahrenheit-32 as f64)/(1.8);
   celsius
 }
 
-fn celsius_to_fahrenheit(celsius: String) -> f64 {
-  let celsius: f64 = celsius.trim().parse().expect("Deve ser um número");
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
   let fahrenheit = 1.8*celsius+32 as f64;
   fahrenheit
 }
 
 fn main() {
-  println!("Digite um número:");
-  let mut num = String::new();
-  std::io::stdin().read_line(&mut num).expect("nao deu pra ler num");
-  println!("Aqui ó: {}", celsius_to_fahrenheit(num));
+  println!("============CONVERSOR DE TEMPERATURA============\nSelecione uma das opções abaixo:\n1. Converter Fahrenheit para Celsius\n2. Converter Celsius para Fahrenheit");
+  let option = get_number_chose();
+
+  if option == 1 {
+    println!("Digite a temperatura em fahrenheit:");
+    let fahrenheit = get_temperature_chose();
+    let celsius = fahrenheit_to_celsius(fahrenheit);
+    println!("A temperatura em celsius é {}ºC", celsius);
+  } else if option == 2 {
+    println!("Digite a temperatura em celsius:");
+    let celsius = get_temperature_chose();
+    let fahrenheit = celsius_to_fahrenheit(celsius);
+    println!("A temperatura em fahrenheit é {}ºF", fahrenheit);
+  }
 }
